a1ccdd6a816ab3722f203a5a3bb037fe8f04f1f7
Mahpara <mah4.para@gmail.com>
2025-09-19 09:54:38 +0100

    feat: Made seperate files and folders for each component and imported them

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import "./App.css";
+import Gallery from "./Components/Gallery/Gallery.jsx";
+import Thumbnail from "./Components/Thumbnail/Thumbnail.jsx";
+import SearchBar from "./Components/SearchBar/SearchBar.jsx";
